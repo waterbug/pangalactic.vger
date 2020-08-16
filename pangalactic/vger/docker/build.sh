548c6a91f85dcd2cecdb24a8cad8fc97e2fe3276
Steve Waterbury <waterbug@pangalactic.us>
2020-08-16 16:50:26 -0400

    Put all docker stuff into a "docker" dir.

---
+++ pangalactic/vger/docker/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+set -euo pipefail
+docker image build -t vger:x.x.x .
---
+++ pangalactic/vger/docker/run_vger_template.sh
@@ -0,0 +1,5 @@
+docker run -it \
+  -v ~/vger_home:/vger_home \
+  -v [local crossbar home]:/vger_home/crossbar \
+  --network=host vger:x.x.x
+
