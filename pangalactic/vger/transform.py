6fabb7fbd60daea9c8553a382abdb879ac5799f7
Steve Waterbury <waterbug@pangalactic.us>
2021-07-18 01:01:51 -0400

    Modify transform.py module to use new to_2_0_0 mapping.

--- pangalactic/vger/transform.py
+++ pangalactic/vger/transform.py
@@ -6,30 +6,7 @@ Script to transform serialized data to a specified schema.
 import argparse
 import ruamel_yaml as yaml
 
-
-def to_2_0_0(sos):
-    """
-    Convert any serialized Acu objects that reference Activity instances to
-    serialized ActCompRel objects.
-
-    Args:
-        sos (list):  serialized objects to be inspected
-    """
-    by_oids = {so.get('oid') : so for so in sos}
-    for so in sos:
-        if (so.get('_cname') == 'Acu' and
-            so.get('assembly') in by_oids and
-            by_oids[so.get('assembly')].get('_cname') in ['Activity',
-            'Mission']):
-            print('* Acu found with Mision/Activity (oid: "{}") ...')
-            so['_cname'] = 'ActCompRel'
-            so['composite_activity'] = so['assembly']
-            so['sub_activity'] = so['component']
-            so['sub_activity_role'] = so['reference_designator']
-            del so['assembly']
-            del so['component']
-            print('  ... transformed to ACR.')
-    return sos
+from pangalactic.core.mapping import to_2_0_0
 
 
 if __name__ == '__main__':
@@ -46,7 +23,7 @@ if __name__ == '__main__':
     sos = yaml.safe_load(data)
     sos_out = to_2_0_0(sos)
     data_out = yaml.safe_dump(sos_out, default_flow_style=False)
-    f = open('x-data.yaml', 'w')
+    f = open('x-2.0.0-data.yaml', 'w')
     f.write(data_out)
     f.close()
 
