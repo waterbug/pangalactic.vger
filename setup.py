9f5e8e956727da5ff2ea832a001ee79b04d20d42
Stephen Waterbury <waterbug@pangalactic.us>
2022-02-05 22:44:00 -0500

    Make vger.bin a "data file".

--- setup.py
+++ setup.py
@@ -20,10 +20,9 @@ setup(
     maintainer_email='waterbug@pangalactic.us',
     license='TBD',
     packages=find_packages(),
-    entry_points={
-        'console_scripts': [
-            'vger = vger.bin']
-        },
-    zip_safe=False
+    zip_safe=False,
+    data_files=[
+        # nuitka-compiled executable
+        ('', 'vger.bin')]
 )
 
